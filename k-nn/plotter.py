863d30aafe471e0f66eaef91530394596414ad27
Chrisser1 <chrisgthomsen0310@gmail.com>
2022-09-16 12:59:50 +0200

    commit

--- k-nn/plotter.py
+++ k-nn/plotter.py
@@ -1,41 +1,49 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from typing import List
-#Points
-x_1 = [2,3]
-y_1 = [4,5]
 
-x_2 = [3,3]
-y_2 = [1,2]
-#Combine values for x and y
-x = np.concatenate((x_1,x_2))
-y = np.concatenate((y_1,y_2))
-#----------------
-#Plot size limit
-plt.xlim(min(x-2), max(x+2))
-plt.ylim(min(y-2), max(y+2))
-plt.grid()
-#----------------
-#Defines plot style
-plt.style.use("seaborn-whitegrid")
-#----------------
-#Makes the plot
-cross = plt.plot(x_1, y_1, "x", markersize=20, markeredgecolor="red")
-circle = plt.plot(x_2, y_2, "o", markersize=20, markeredgecolor="black", markerfacecolor="green")
-plt.show(cross,circle)
-
+#Funktion for x,y-værdier
 def plot (type1:List[List[int]],type2: List[List[int]]) -> None:
   #type1[0] giver liste over x af type 1
+  type1[0]
   #type1[1] giver liste over y af type 1
+  type1[1]
   #type2[0] giver liste over x af type 2
+  type2[0]
   #type2[1] giver liste over y af type 2
-
+  type2[1]
+  #----------------
+  #Combine values for x and y
+  x = np.concatenate((type1[0],type2[0]))
+  y = np.concatenate((type1[1],type2[1]))
+  #----------------
+  #Plot size limit
+  plt.xlim(min(x-2), max(x+2))
+  plt.ylim(min(y-2), max(y+2))
+  plt.grid()
+  #----------------
+  #Defines plot style
+  plt.style.use("seaborn-whitegrid")
+  #----------------
+  #Makes point plot
+  cross = plt.plot(type1[0], type1[1], "x", markersize=20, markeredgecolor="red")
+  circle = plt.plot(type2[0], type2[1], "o", markersize=20, markeredgecolor="black", markerfacecolor="green")
   #printer et plot over hvor de to typer er
-  pass
+  plt.show(cross,circle)
+
 
 def plotK (type1:List[List[int]]) -> None:
   #type1[0] giver liste over x
+  type1[0]
   #type1[1] giver liste over y
-
+  type1[1]
+  #----------------
+  #Plot size limit
+  plt.xlim(min(type1[0]-2), max(type1[0]+2))
+  plt.ylim(min(type1[1]-2), max(type1[1]+2))
+  plt.grid()
+  #----------------
+  #Makes lines between each k-point
+  plt.plot(type1[0], type1[1], "o--", linewidth=5, markersize=12, color="blue", markerfacecolor="pink")
   #printer et plot over hvordan ændringer i k pævirker antal korekte
-  pass
+  plt.show()
