0e4c1d5833507879854fa076bc3721c8018b86d9
Chrisser1 <chrisgthomsen0310@gmail.com>
2022-11-03 13:23:40 +0100

    e

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from knn.testKNN import quick, quickData, quickRand, quickTrueData
 
 while True:
-  quick()
+  # quick()
   # quickRand(100,300,0,120,25)
-  # quickTrueData()
+  quickTrueData()
 
   if input ("done? ") == "yes":
     break
